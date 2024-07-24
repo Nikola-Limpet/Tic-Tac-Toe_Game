9775f3fd42429f06afadb5d12a8c513cdf74d11a
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-07-24 07:12:13 +0000

    Jul 24, 2024, 2:12 PM

--- main.js
+++ main.js
@@ -15,3 +15,17 @@ let gameState = {
 };
 
 // Write a function to render the game board based on the game state array. This function should update the innerHTML of the board's cells to show an "X" or "O" based on the array's values.
+
+function renderGame() {
+  const player1 = document.querySelector("#player1");
+  const player2 = document.querySelector("#player2");
+
+  player1.innerHTML = `${gameState.player1.name}: ${gameState.player1.score}`;
+  player2.innerHTML = `${gameState.player2.name}: ${gameState.player2.score}`;
+
+  const cells = document.querySelectorAll(".cell");
+
+  cells.forEach((cell, index) => {
+    cell.innerHTML = gameState[index];
+  });
+}
